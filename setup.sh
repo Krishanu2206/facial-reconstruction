f3f317628b9bffd8b6ca03700aef727dc4fcab6a
Arka Dash <dasharka05@gmail.com>
2024-09-23 21:10:01 +0530

    setup updated

--- setup.sh
+++ setup.sh
@@ -2,18 +2,48 @@
 
 set -e
 
+if ! command -v python3 &> /dev/null; then
+    echo "Python3 is not installed. Please install Python3 first."
+    exit 1
+fi
+
+# Create a virtual environment named 'venv' if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    echo "Virtual environment 'venv' created."
+else
+    echo "Virtual environment 'venv' already exists."
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+echo "Virtual environment 'venv' activated."
+
+# Check if requirements.txt exists
+if [ -f "requirements.txt" ]; then
+    touch logs.txt
+    pip install -r requirements.txt >> logs.txt
+    echo "Dependencies installed from requirements.txt."
+else
+    echo "requirements.txt not found. Please make sure it exists."
+    deactivate
+    exit 2
+fi
+
 if [ ! -d "data" ]; then
     mkdir data
     echo "data folder created"
-    echo "now download the data"
-    exit 1
 fi
 
 cd data
 
 zip_file="scface.zip"
 if [ ! -f "$zip_file" ]; then
-    echo "$zip_file not found. Please download the data and try again."
+    echo "$zip_file not found. Downloading..."
+    gdown --id 1mxbAgil0-Lbka9FnNTRyrxHKlVB0vgGQ
+    echo "Data downloaded"
+    unzip "$zip_file" .
+    echo "Data unzipped"
 else
     unzip "$zip_file" .
     echo "Data unzipped"
@@ -42,3 +72,7 @@ train_dir="processed/train"
 test_dir="processed/test"
 
 python data_setup.py --lowResImagesPath "$lowResImagesPath" --highResImagesPath "$highResImagesPath" --train_dir "$train_dir" --test_dir "$test_dir" || { echo "Training failed"; exit 6; }
+
+rm -r $rgb_heatmap_low $rgb_heatmap_high
+
+echo "Setup complete"
